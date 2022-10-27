ea28eb1f07658fdc0639d27fae7b56e7b9a1ca9b
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-10-27 11:01:32 -0700

    update install and webpack.config

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,25 +4,15 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
+    butInstall.style.visibility = 'visible';
+    textHeader.textContent = 'Click the button to install!';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async (event) => {
-    const promptEvent = window.deferredPrompt;
-    // console.log(promptEvent)
-    if (!promptEvent) {
-        return;
-    }
-
-    // Show prompt
-    promptEvent.prompt();
-
-    // Reset the deferred prompt variable, it can only be used once.
-    window.deferredPrompt = null;
-
-    butInstall.classList.toggle('hidden', true);
+butInstall.addEventListener('click', (event) => {
+    event.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
 });
 
 // TODO: Add an handler for the `appinstalled` event
